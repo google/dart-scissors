d99e45999ac8c4eb27dd9f3e4bd3d278fef7dfca
Olivier Chafik <ochafik@google.com>
2015-11-01 11:23:47 +0100

    Support parsing function in settings

--- lib/src/setting.dart
+++ lib/src/setting.dart
@@ -5,6 +5,7 @@ class _Setting<T> {
   final String comment;
   final T debugDefault;
   final T releaseDefault;
+  final Function parser;
   bool _read = false;
   T _value;
   T get value {
@@ -13,13 +14,20 @@ class _Setting<T> {
   }
 
   _Setting(this.key,
-      {this.comment, T defaultValue, T debugDefault, T releaseDefault})
+      {this.comment, T defaultValue, T debugDefault, T releaseDefault,
+       T this.parser(String s)})
       : this.debugDefault = debugDefault ?? defaultValue,
         this.releaseDefault = releaseDefault ?? defaultValue;
 
   read(Map config, bool isDebug) {
     checkState(!_read);
     _read = true;
-    _value = config[key] ?? (isDebug ? debugDefault : releaseDefault);
+    var value = config[key];
+    if (value == null) {
+      _value = isDebug ? debugDefault : releaseDefault;
+    } else {
+      _value = parser != null ? parser(value) : value;
+    }
+    // print("VALUE[$key] = $_value (${config[key]})");
   }
 }
