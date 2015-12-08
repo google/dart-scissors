a34dacb8ed3f17423b2fb36a689301a1a5547702
MonamAgarwal <monama@monama-macbookpro.roam.corp.google.com>
2015-12-08 17:11:51 +0000

    Removed setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -d "$HOME/bin" ]; then
-  echo 'creating directory ${HOME}/bin' 
-  mkdir $HOME/bin
-fi
-svn checkout http://cssjanus.googlecode.com/svn/trunk/ $HOME/bin/cssjanus
-echo 'checkout cssjanus at '$HOME'/bin/cssjanus'
