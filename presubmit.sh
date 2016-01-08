c22bec131f848857c2401241a9c3f21b0d4d195b
Olivier Chafik <ochafik@google.com>
2016-01-08 11:11:10 +0000

    Cleanup presubmit + analyze!

--- presubmit.sh
+++ presubmit.sh
@@ -3,18 +3,32 @@ set -eu
 
 pub get
 
-#pub run test
-for test in `find test -name '*.dart'` ; do
-  echo "TESTING $test"
-  TRAVIS=true pub run $test
-done
-pub publish --dry-run
+function run_analyzer() {
+  echo "Running the analyzer"
+  dartanalyzer --fatal-warnings `find bin -name '*.dart'` `find lib -name '*.dart'`
+}
 
-echo "+ pub run dart_style:format ..."
-pub run dart_style:format -w \
-  `find lib -name '*.dart'` \
-  `find example -name '*.dart'` \
-  `find test -name '*.dart'` | ( grep -v "^Unchanged " || true )
+function run_tests() {
+  echo "Running tests"
+  # TODO(ochafik): `pub run test` again? (sometimes not reliable)
+  for test in `find test -name '*.dart'` ; do
+    echo "TESTING $test"
+    TRAVIS=true pub run $test
+  done
+}
+
+function run_formatter() {
+  echo "Running the formatter"
+  pub run dart_style:format -w \
+    `find lib -name '*.dart'` \
+    `find example -name '*.dart'` \
+    `find test -name '*.dart'` | ( grep -v "^Unchanged " || true )
+}
+
+run_analyzer
+run_tests
+run_formatter
+pub publish --dry-run
 
 if (( "${TEST_EXAMPLES:-1}" )); then
   example/test_example.sh permutations
