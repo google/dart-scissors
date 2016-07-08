6a822534a597ddbdf61a199d617f4290f92cbf3b
Andrew Shulaev <ddrone@google.com>
2016-07-08 13:54:50 +0100

    Add a test with fake png file and multiple directories

--- test/image_dart_compiling/transformer_test.dart
+++ test/image_dart_compiling/transformer_test.dart
@@ -10,14 +10,24 @@ main() {
   ''';
   const iconSvgData =
       'ICAgIDw/eG1sIHZlcnNpb249IjEuMCIgZW5jb2Rpbmc9InV0Zi04Ij8+CiAgICA8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPgogICAgICA8cmVjdCB4PSIwIiB5PSIwIiBoZWlnaHQ9IjEwIiB3aWR0aD0iMTAiIHN0eWxlPSJzdHJva2U6IzAwZmYwMDsgZmlsbDogI2ZmMDAwMCIvPgogICAgPC9zdmc+CiAg';
+  const encodedSvg = 'data:image/svg+xml;base64,$iconSvgData';
 
   List<List> phases = [
     [new DartImageCompiler.asPlugin()]
   ];
-  testPhases('image_dart_transformer generates images.dart file', phases, {
-    'web|icon.svg': iconSvg
+  testPhases(
+      'image_dart_transformer generates images.dart file',
+      phases,
+      {'web|icon.svg': iconSvg},
+      {'web|images.dart': 'const icon = "$encodedSvg";\n'});
+
+  testPhases(
+      'image_dart_transformer groups images based on directory', phases, {
+    'web|pictures1/icon.svg': iconSvg,
+    'web|pictures2/fake_image.png': 'fake image contents'
   }, {
-    'web|images.dart':
-        'const icon = "data:image/svg+xml;base64,$iconSvgData";\n'
+    'web|pictures1/images.dart': 'const icon = "$encodedSvg";\n',
+    'web|pictures2/images.dart':
+        'const fake_image = "data:image/png;base64,ZmFrZSBpbWFnZSBjb250ZW50cw==";\n'
   });
 }
